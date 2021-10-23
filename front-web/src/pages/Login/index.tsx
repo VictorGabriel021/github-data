ba4c75267ef6d1858bbce41a35186902a82fbe81
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-10-23 16:09:27 -0300

    Adding login layout

---
+++ front-web/src/pages/Login/index.tsx
@@ -0,0 +1,63 @@
+import './styles.scss';
+import { GoMarkGithub } from 'react-icons/go';
+import { RiArrowRightLine } from 'react-icons/ri';
+import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
+import { makeRequest } from '../../core/utils/request';
+
+type FormData = {
+    username: string;
+}
+
+const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        makeRequest({ url: `https://github.com/${data.username}` })
+            .then(() => {
+                toast.success("Login com sucesso", {
+                    position: toast.POSITION.BOTTOM_RIGHT
+                });
+            })
+            .catch(() => {
+                toast.error("Erro ao efetuar o Login", {
+                    position: toast.POSITION.BOTTOM_RIGHT
+                });
+            });
+    }
+
+    return (
+        <div className="container">
+            <div className="login-content">
+                <div className="text-center mb-5">
+                    <GoMarkGithub className="github-icon" />
+                </div>
+                <form className="login-form form-floating" onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Usuário"
+                        {...register('username', {
+                            required: "Campo obrigatório"
+                        })
+                        }
+                    />
+                    <label htmlFor="floatingInputInvalid" className="required-field">
+                        {errors.username && (
+                            <>
+                                {errors.username.message}
+                            </>
+                        )}
+                    </label>
+                    <div className="d-grid">
+                        <button type="submit" className="btn btn-warning login-button">
+                            ENTRAR <RiArrowRightLine className="arrow-right" />
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
