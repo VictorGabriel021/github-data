ed0d110b432c27c24c166a959880fb99f5c8fd0d
VictorGabriel <victorgabriel.cipa@gmail.com>
2024-03-04 10:16:45 -0300

    chore: remove text

--- front-web/src/pages/Followers/Follower/index.tsx
+++ front-web/src/pages/Followers/Follower/index.tsx
@@ -49,8 +49,6 @@ const Follower = () => {
                 />
               ))}
 
-              <p>opa</p>
-
               {!!followerData?.data?.length && (
                 <Pagination
                   pageCount={pageCount}
